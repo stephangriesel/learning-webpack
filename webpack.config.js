7d7f4ea8b428b77ccfd3300b427c934074561e4a
Stephan Griesel <sgriesel@gmail.com>
2017-03-31 16:52:11 +0200

    learning webpack

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/app.js',
+    output: {
+        path: "dist",
+        filename: "app.bundle.js"
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Project Demo',
+            minify: {
+                collapseWhitespace: true
+            },
+            hash: true,
+            template: './src/index.ejs', // Load a custom template (ejs by default see the FAQ for details)
+        })
+    ]
+}
